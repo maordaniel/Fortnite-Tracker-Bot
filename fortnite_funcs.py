61e06d113ff20a45f8a61aa0512115e6b3cb8766
maordaniel <47079236+maordaniel@users.noreply.github.com>
2020-02-15 20:46:57 +0200

    Update fortnite_funcs.py

--- fortnite_funcs.py
+++ fortnite_funcs.py
@@ -17,6 +17,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 user = {}
 
+earth = emojize(':earth_africa:', use_aliases=True)
 vi = emojize(":white_check_mark:", use_aliases=True)
 mag = emojize(":mag:", use_aliases=True)
 mag_right = emojize(":mag_right:", use_aliases=True)
