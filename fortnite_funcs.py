20a1b470360ec22b70a077facfba3b0bbf8d0675
maordaniel <47079236+maordaniel@users.noreply.github.com>
2020-02-15 20:46:09 +0200

    Update fortnite_funcs.py

--- fortnite_funcs.py
+++ fortnite_funcs.py
@@ -14,7 +14,9 @@ updater = Updater(token='telegram token', use_context=True)
 dispatch = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
+
 user = {}
+
 vi = emojize(":white_check_mark:", use_aliases=True)
 mag = emojize(":mag:", use_aliases=True)
 mag_right = emojize(":mag_right:", use_aliases=True)
